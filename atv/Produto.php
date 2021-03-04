8ce6ebcfb21dd9823c8ae106b4a2ca81af0587a7
AndreyMNS <63016741+AndreyMNS@users.noreply.github.com>
2021-03-04 18:12:37 -0300

    Add files via upload

---
+++ atv/Produto.php
@@ -0,0 +1,66 @@
+<html>
+    <body>
+        <h1> Produto </h1>
+        </table>
+        <h2> Cadastro de Produto </h2>
+        <form>
+        Nome: <input type = "text" name = "nome" size = 32 maxlenght = 20/>
+        <br/>
+          Cod: <input type = "num" name = "cod" size = 32 maxlenght = 10/>
+        <br/>
+           Qtd:<input type = "num" name = "qtd" size = 32 maxlenght = 20/>
+        <br/>
+            Pc :<input type = "text" name = "pc" size = 32 maxlenght = 20/>
+        <br/>
+        <br/>
+        <input type = "submit" name = "botaoEnviar" value = "Cadastrar"/>  
+        </form>
+        <p> Cadastro de produto contendo informações sobre o mesmo </p>
+        <h2> Lista de Produtos </h2>
+        <table>
+            <tr>
+                <th> Cod </th>
+                <th> Nome</th>
+                <th> Qtd      </th>
+                <th> Preço Unitário   </th>
+                <th> Preço Total    </th>
+            </tr>
+            <tr>
+                <td> 0 </td>
+                <td> Café </td>
+                <td> 5000 </td>
+                <td> 4,90 </td>
+                <td> null </td>
+            </tr>
+            <tr>
+                <td> 1 </td>
+                <td> Açúcar </td>
+                <td> 3500 </td>
+                <td> 3,42 </td>
+                <td> null </td>
+            </tr>
+            <tr>
+                <td> 2 </td>
+                <td> Leite </td>
+                <td> 2000 </td>
+                <td> 1,96 </td>
+                <td> null </td>
+            </tr>
+            <tr>
+                <td> 3 </td>
+                <td> Arroz </td>
+                <td> 3200 </td>
+                <td> 1,45 </td>
+                <td> null </td>
+            </tr>
+            <tr>
+                <td> 4 </td>
+                <td> Feijão </td>
+                <td> 6000 </td>
+                <td> 4,60 </td>
+                <td> null  </td>
+            </tr>
+            
+        
+    </body>
+</html>
